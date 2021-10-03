1644574c0b959d95ccca54710f83a1855fcfb2d4
Elias <elikapeluschnik@gmail.com>
2021-10-03 18:04:27 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Write your code here!
+
+main.remove(); 
+
+const newHeader = document.createElement('h1')
+    newHeader.setAttribute("id", "victory")
+
+    // document.getElementById("victory").append("YOUR-NAME is the champion");
+
+    newHeader.innerHTML = "<h1> Eli is the champion </h1>"
+
+     
